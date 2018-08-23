5dc828a8c63ad2e5b2be4f1d5ffe313e08446f1d
arseniy_bulkin <ruslancheg18@gmail.com>
2018-08-24 00:15:18 +0500

    Заново начал писать логику алгоритма стрельбы компьютера

--- src/js/main.js
+++ src/js/main.js
@@ -8,17 +8,12 @@ var app = new Vue({
 	data: {
 		gamers: {
 			computer: {
-				fleet: Array,
-				field: emptyField(),
-				fleetSize: 0,
-				winner: false
+				// shootMatrixAround: [],
+				// shootMatrixAI: [],
+				// shootMatrix: emptyField(),
+				luckyShots: ''
 			},
-			user: {
-				fleet: Array,
-				field: emptyField(),
-				fleetSize: 0,
-				winner: false
-			}
+			user: {}
 		},
 		isComputerTurn: true,
 		gameSteps: {
@@ -27,7 +22,66 @@ var app = new Vue({
 			isFinished: false
 		}
 	},
+	beforeMount() {
+		for (let gamer in this.gamers) {
+			this.$set(this.gamers[gamer], 'fleet', Array);
+			this.$set(this.gamers[gamer], 'field', emptyField());
+			this.$set(this.gamers[gamer], 'fleetSize', 0);
+			this.$set(this.gamers[gamer], 'isWinner', false);
+		}
+
+		// this.shootMatrixexCreate();
+	},
 	methods: {
+		compareRandom (a, b) {
+			return Math.random() - 0.5;
+		},
+		shootMatrixexCreate() {
+			var startPoints = [
+				[ [7,1], [3,1], [1,3], [1,7] ],
+				[ [4,1], [8,1], [10,3], [10,7] ]
+			];
+
+			for (var i = 1; i < 11; i++) {
+				for(var j = 1; j < 11; j++) {
+					this.gamers.computer.shootMatrix.push([i, j]);
+				}
+			}
+
+			for (let i = 0, length = startPoints.length; i < length; i++) {
+
+				var arr = startPoints[i];
+				for (var j = 0, lh = arr.length; j < lh; j++) {
+
+					var x = arr[j][0],
+						y = arr[j][1];
+
+					switch(i) {
+						case 0:
+							while(x <= 10 && y <= 10) {
+								this.gamers.computer.shootMatrixAI.push([x,y]);
+								x = (x <= 10) ? x : 10;
+								y = (y <= 10) ? y : 10;
+								x++; y++;
+							};
+							break;
+
+						case 1:
+							while(x >= 1 && x <= 10 && y <= 10) {
+								this.gamers.computer.shootMatrixAI.push([x,y]);
+								x = (x >= 1 && x <= 10) ? x : (x < 1) ? 1 : 10;
+								y = (y <= 10) ? y : 10;
+								x--; y++;
+							};
+							break;
+					}
+				}
+			}
+
+
+			this.gamers.computer.shootMatrix.sort(this.compareRandom);
+			this.gamers.computer.shootMatrixAI.sort(this.compareRandom);
+		},
 		addShips (gamer) {
 			gamer.field = emptyField();
 			gamer.fleetSize = 0;
@@ -37,13 +91,13 @@ var app = new Vue({
 			fleetData.forEach((ship, i, arr) => {
 				let shipsQuantity = ship.quantity;
 
-				gamer.fleetSize += ship.quantity;
+				gamer.fleetSize += ship.quantity * ship.size;
 
 				if (ship.size == 4) {
-					this.createShip(gamer, ship, this.getCoordinates(ship.size));
+					this.createShip(gamer, ship, this.createCoordinates(ship.size));
 				} else {
 					while (shipsQuantity) {
-						var coords = this.getCoordinates(ship.size);
+						var coords = this.createCoordinates(ship.size);
 						if (this.checkEmptyCell(gamer, ship, coords)) {
 							this.createShip(gamer, ship, coords);
 							shipsQuantity--
@@ -52,7 +106,7 @@ var app = new Vue({
 				}
 			});
 		},
-		getCoordinates(n) {
+		createCoordinates(n) {
 			let row, col, direction = this.random(0,1);
 			if (direction == 1) {
 					row = this.random(1,10);
@@ -97,28 +151,20 @@ var app = new Vue({
 			return isAvailable;
 		},
 		createShip (gamer, ship, coords) {
-			var coordinates = [],
-					row = coords.row,
+			var row = coords.row,
 					col = coords.col,
 					direction = coords.direction;
 
 			if (direction == 1) {
 				for (let i = 0; i < ship.size; i++) {
 					this.$set(gamer.field[row], (col + i), ship.size);
-					coordinates.push([row, col + i]);
 				}
 			} else {
 				for (let i = 0; i < ship.size; i++) {
 					this.$set(gamer.field[row + i], col, ship.size);
-					coordinates.push([row + i, col]);;
 				}
 			}
 
-			gamer.fleet.push({
-				ship: ship.size,
-				coords: coordinates,
-				isAlive: true
-			});
 		},
 		random(min, max) {
 			var rand = min - 0.5 + Math.random() * (max - min + 1)
@@ -132,7 +178,7 @@ var app = new Vue({
 			this.$set(this.gameSteps, 'newGame', true);
 		},
 		startGame () { 
-			if (this.random(0, 1) == 1) {
+			if (this.random(1, 1) == 1) {
 				this.isComputerTurn = false;
 			}
 
@@ -147,43 +193,68 @@ var app = new Vue({
 
 			this.$set(this.gameSteps, 'isStarting', true);
 		},
-		userShot (row, cellValue, cellIndex) {
+		userShot (row, col, value) {
+
 			if (!this.isComputerTurn) {
-				if (cellValue > 0) {
-					this.$set(row, cellIndex, -3);
-					this.checkEnemyFleetBalance(this.gamers.computer);
+				if (value > 0) {
+					this.$set(row, col, -3);
+					this.checkFinishGame(this.gamers.computer);
 				} else {
-					this.$set(row, cellIndex, -2);
-					// Выстрел компьютера
-					this.computerShot();
+					this.$set(row, col, -1);
+					this.isComputerTurn = true;
+
+					this.serviceMessage = 'Промах';
+					
+					setTimeout(() => {
+						this.computerShot();
+					}, 500);
+					
 				}
 			}
 		},
-		checkEnemyFleetBalance (gamer) {
+		computerShot() {
+			if (this.gamers.computer.luckyShots !== '') {
+				this.computerAfterResultativeShot();
+			} else {
+				let row = this.random(1, 10),
+						col = this.random(1, 10),
+						value = this.gamers.user.field[row][col];
+
+
+				if (value > 0) {
+						this.$set(this.gamers.user.field[row], col, -3);
+						
+						this.$set(this.gamers.user.field[row + 1], (col + 1), -1);
+						this.$set(this.gamers.user.field[row + 1], (col - 1), -1);
+						this.$set(this.gamers.user.field[row - 1], (col + 1), -1);
+						this.$set(this.gamers.user.field[row - 1], (col - 1), -1);
+
+						this.serviceMessage = 'Компьютер попал';
+						this.checkFinishGame(this.gamers.user);
+						this.setLuckyShots(row, col);
+						this.gamers.computer.luckyShots.totalHits++;
+				} else if (value <= -1) {
+					this.computerShot();
+				} else {
+					this.$set(this.gamers.user.field[row], col, -1);
+					this.isComputerTurn = false;
+					this.serviceMessage = 'Компьютер промазал';
+				}
+			}
+		},
+		computerAfterResultativeShot () {
+
+		},
+		checkFinishGame (gamer) {
+			gamer.fleetSize--;
+
 			if (gamer.fleetSize === 0) {
 				this.gameSteps.isFinished = true;
-			}
-		},
-		computerShot () {
-			let row = this.random(1, 10),
-					col = this.random(1, 10),
-					me = this;
-
-			if (this.luckyShot === '') {
-				if (this.userField[row][col] > 0) {
-					this.resultativeShot(row, col);
+				if (gamer == 'user') {
+					this.serviceMessage = 'Вы проиграли'
 				} else {
-					if (this.userField[row][col] <= -2) {
-						setTimeout(function () {
-							me.computerShot();
-						}, 500);
-					} else {
-						this.serviceMessage = 'Компьютер промахнулся';
-						this.$set(this.userField[row], col, -2);
-					}
+					this.serviceMessage = 'Вы выиграли'
 				}
-			} else {
-				this.computerAimingShot();
 			}
 		},
 		exitGame () {
@@ -191,5 +262,14 @@ var app = new Vue({
 				this.serviceMessage = 'Вы проиграли'
 			}
 		},
+		setLuckyShots (col, row) {
+			this.gamers.computer.luckyShots = {
+				totalHits: 0,
+				firstHit: {},
+				nextHit: {},
+				kx: 0,
+				ky: 0
+			};
+		}
 	}
 });
